aac639496ed2cd7d13f8d6cddf20b5d952bf263a
zobeir-rigi <rigi.zobir@yahoo.com>
2024-01-30 21:23:29 +0000

    sendWatsappMessage is working successfully

--- readData.js
+++ readData.js
@@ -3,9 +3,8 @@ const twilio = require('twilio');
 
 // Twilio credentials
 const accountSid = 'ACf720f5e52dd5484daea68e3e47353f88';
-const authToken = '[AuthToken]';
+const authToken = '7b589d15f8641b6f0f9266d528f5c33d';
 const client = require('twilio')(accountSid, authToken);
-
 
 function readUserInfo(filePath) {
     try {
@@ -17,10 +16,24 @@ function readUserInfo(filePath) {
         return null;
     }
 }
+function sendWhatsAppMessage(user) {
+    const messageBody = `Hello, ${user.name} from ${user.country}`;
+    client.messages
+    .create({
+        body: messageBody,
+        from: 'whatsapp:+14155238886',
+        to: `whatsapp:${user.celphone}`
+    })
+    .then(message => console.log(`Message sent to ${user.name} from ${user.country}. Message SID: ${message.sid}`))
+    .catch(error => console.error('Error sending message:', error));
+}
 
 const filePath = 'userInfo.json';
 const userInfoData = readUserInfo(filePath);
 
-if (userInfoData) {
+if (userInfoData && userInfoData.length > 0) {
     console.log('User info read successfully:', userInfoData);
+    sendWhatsAppMessage(userInfoData[0]);  
+} else {
+    console.error('No user data found in the JSON file.');
 }
